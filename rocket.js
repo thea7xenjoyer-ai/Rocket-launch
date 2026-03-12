b66c49ce613a8d72c07229ccb60637de0ee5b650
Hearz <thea7xenjoyer@gmail.com>
2026-03-12 13:40:50 +0200

    Add files via upload

---
+++ rocket.js
@@ -0,0 +1,37 @@
+var timer = null;
+var countdownNumber = 10;
+
+var changeState = function (state) {
+    document.body.className = 'body-state'+
+        state;
+    clearInterval(timer);
+    countdownNumber = 10;
+
+
+
+    if (state == 2) {
+        timer = setInterval (function () {
+        document.getElementById ('countdown').
+        innerHTML = countdownNumber;
+        countdownNumber = countdownNumber -1;
+        
+        if (countdownNumber <= -1){
+            changeState(3);
+        };
+            }, 1000);
+        } else if (state == 3) {
+            var success = setTimeout (function (){
+                var randomNumber = Math.round(Math.random()*10);
+
+                console.log ('randomNumber:', randomNumber)
+
+                if (randomNumber > 5) {
+                    changeState(4);
+                } else {
+                    changeState(5);
+                }
+            }, 1000);
+
+            
+        }
+}
